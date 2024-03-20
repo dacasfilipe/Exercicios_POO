619436cf5965ed146472a23d1c90db87ca20e53e
ClaytonAndrade <clayton_m_andrade@hotmail.com>
2024-03-20 20:40:05 -0300

    Classe Carro add

---
+++ src/Carro/Main.java
@@ -0,0 +1,17 @@
+package Carro;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        Scanner leia = new Scanner(System.in);
+        System.out.println("Digite a marca:");
+        carro.marca = leia.next();
+        System.out.println("Digite a modelo:");
+        carro.modelo = leia.next();
+        System.out.println("Digite o Ano:");
+        carro.ano = leia.nextInt();
+        carro.exibirDetalhes();
+    }
+}
