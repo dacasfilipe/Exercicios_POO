517be47e88c39cffe99a59f47ec10a58771fa3c7
ClaytonAndrade <clayton_m_andrade@hotmail.com>
2024-03-27 19:10:37 -0300

    add class Estudante

--- src/Estudante/Main.java
+++ src/Estudante/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner leia = new Scanner(System.in);
         Estudante estudante = new Estudante();
+        estudante.nome = "Mario";
         System.out.println("Digite a nota 1:");
         double nota1 = leia.nextDouble();
         estudante.adicionarNota(nota1);
